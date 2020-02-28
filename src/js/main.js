f20b1e28343939141b76ae5915b1b5361c8c8fb7
uzinok <uzinok@yandex.ru>
2020-02-28 15:24:51 +0400

    youtube-video

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,65 @@
-window.onload = function () {};
+window.onload = function () {
+
+    // для тех у кого отключен js нужно не забывать
+    document.querySelector('.nojs').classList.remove('nojs');
+
+    // загрузка видео с youtube
+    findVideos();
+
+};
+
+function findVideos() {
+    if (document.querySelectorAll('.video')) {
+
+        let videos = document.querySelectorAll('.video');
+
+        for (let i = 0; i < videos.length; i++) {
+            setupVideo(videos[i]);
+        }
+
+    }
+}
+
+function setupVideo(video) {
+    let link = video.querySelector('.video__link');
+    let media = video.querySelector('.video__media');
+    let button = video.querySelector('.video__button');
+    let id = parseMediaURL(media);
+
+    link.removeAttribute('href');
+    video.classList.add('video--enabled');
+
+    video.addEventListener('click', function () {
+        let iframe = createIframe(id);
+
+        link.style.display = "none";
+        button.style.display = "none";
+        video.appendChild(iframe);
+    });
+
+}
+
+function parseMediaURL(media) {
+    let regexp = /https:\/\/i\.ytimg\.com\/vi\/([a-zA-Z0-9_-]+)\/maxresdefault\.jpg/i;
+    let url = media.src;
+    let match = url.match(regexp);
+
+    return match[1];
+}
+
+function createIframe(id) {
+    let iframe = document.createElement('iframe');
+
+    iframe.setAttribute('allowfullscreen', '');
+    iframe.setAttribute('allow', 'autoplay');
+    iframe.setAttribute('src', generateURL(id));
+    iframe.classList.add('video__media');
+
+    return iframe;
+}
+
+function generateURL(id) {
+    let query = '?rel=0&showinfo=0&autoplay=1';
+
+    return 'https://www.youtube.com/embed/' + id + query;
+}
